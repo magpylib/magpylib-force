aff49b6ec9520dc29cf80cbd2992293a235a47c6
mortner <magpylib@gmail.com>
2024-04-19 23:29:27 +0200

    fix version

--- mforce/__init__.py
+++ mforce/__init__.py
@@ -6,7 +6,7 @@ The mforce package sits on top of magpylib for force computation
 """
 
 # module level dunders
-__version__ = "0.1.2dev"
+__version__ = "0.1.3"
 __author__ = "SAL"
 __all__ = [
     "getFTcube",
