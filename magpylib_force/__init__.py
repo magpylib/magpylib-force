9a6017e5c9769d5a4092c01f7fe08113fcf3bb35
mortner <magpylib@gmail.com>
2024-10-30 09:48:02 +0100

    version bump to 031

--- magpylib_force/__init__.py
+++ magpylib_force/__init__.py
@@ -7,7 +7,7 @@ can be found in the Magylib documentation.
 from magpylib_force.force import getFT
 
 # module level dunders
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 __author__ = "The Magpylib Team"
 __all__ = [
     "getFT",
